55180c8aada9ec121a1da5a4470bd6e8c2780093
phuongnguyenlaaithe <phuongnguyen2522003@gmail.com>
2024-10-23 23:01:08 +0700

    feat: add user router

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRoute.js';
 
 //App Config
 const app = express();
@@ -15,6 +16,8 @@ app.use(cors());
 app.use(express.json());
 
 //API Endpoints
+app.use('/api/users', userRouter);
+
 app.get('/', (req, res) => {
     res.send('API Working');
 })
