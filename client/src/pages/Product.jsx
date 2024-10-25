9caf98168b4207646580faf7ecef38c836f5af1e
Phuc_Phung <minhphuchc1308@gmail.com>
2024-10-25 13:29:10 +0700

    add related product

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { id } = useParams();
@@ -94,6 +95,8 @@ const Product = () => {
         </div>
 
       </div>
+
+      <RelatedProducts category={product.category} subCategory={product.subCategory}/>
 
     </div>
   ) : <div className="opacity-0"></div>
