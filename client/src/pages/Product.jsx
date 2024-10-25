2eed4c256374c6e627b3b9202ed771224be4ea0a
Phuc_Phung <minhphuchc1308@gmail.com>
2024-10-25 12:36:18 +0700

    add description and review

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -77,7 +77,24 @@ const Product = () => {
         </div>
       </div>
 
-      
+      <div className="mt-20">
+
+        <div className="flex">
+          <b className="py-3 px-4 border text-sm">Description</b>
+          <p className="py-3 px-4 border text-sm">Reviews (66)</p>
+        </div>
+
+        <div className="flex flex-col gap-4 border p-5 text-sm text-gray-500 ">
+          <p>
+          An e-commerce website is an online platform that facilitates the buying and selling of products or services over the internet. It serves as a virtual marketplace where businesses and individuals can showcase their products, interact with customers, and conduct transactions without the need for a physical presence. E-commerce websites have gained immense popularity due to their convenience, accessibility, and the global reach they offer.
+          </p>
+          <p>
+          E-commerce websites typically display products or services along with detailed descriptions, images, prices, and any available variations (e.g., sizes, colors). Each product usually has its own dedicated page with relevant information.
+          </p>
+        </div>
+
+      </div>
+
     </div>
   ) : <div className="opacity-0"></div>
 };
