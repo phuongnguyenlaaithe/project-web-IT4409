3525c6cf3fad7467df56cdf5dab8b50bef619360
daophuc2k3 <dodaophuc1111@gmail.com>
2024-11-01 10:54:51 +0700

    update collection page

--- client/src/pages/Collection.jsx
+++ client/src/pages/Collection.jsx
@@ -1,104 +1,152 @@
-import ProductList from "../components/ProductList"
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import { assets } from '../assets/assets';
+import Title from '../components/Title';
+import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
+
+  const { products, search, showSearch } = useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false);
+  const [filterProducts, setFilterProducts] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [subCategory, setSubCategory] = useState([]);
+  const [sortType, setSortType] = useState('relavent')
+
+  const toggleCategory = (e) => {
+
+    if (category.includes(e.target.value)) {
+      setCategory(prev => prev.filter(item => item !== e.target.value))
+    }
+    else {
+      setCategory(prev => [...prev, e.target.value])
+    }
+
+  }
+
+  const toggleSubCategory = (e) => {
+
+    if (subCategory.includes(e.target.value)) {
+      setSubCategory(prev => prev.filter(item => item !== e.target.value))
+    }
+    else {
+      setSubCategory(prev => [...prev, e.target.value])
+    }
+  }
+
+  const applyFilter = () => {
+
+    let productsCopy = products.slice();
+
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+    }
+
+    if (category.length > 0) {
+      productsCopy = productsCopy.filter(item => category.includes(item.category));
+    }
+
+    if (subCategory.length > 0) {
+      productsCopy = productsCopy.filter(item => subCategory.includes(item.subCategory))
+    }
+
+    setFilterProducts(productsCopy)
+
+  }
+
+  const sortProduct = () => {
+
+    let fpCopy = filterProducts.slice();
+
+    switch (sortType) {
+      case 'low-high':
+        setFilterProducts(fpCopy.sort((a, b) => (a.price - b.price)));
+        break;
+
+      case 'high-low':
+        setFilterProducts(fpCopy.sort((a, b) => (b.price - a.price)));
+        break;
+
+      default:
+        applyFilter();
+        break;
+    }
+
+  }
+
+  useEffect(() => {
+    applyFilter();
+  }, [category, subCategory, search, showSearch, products])
+
+  useEffect(() => {
+    sortProduct();
+  }, [sortType])
+
   return (
-   <main className="mx-auto pt-6 border-t border-gray-300">
-        <section aria-labelledby="products-heading" className="pb-24 pt-6">
-          <h2 id="products-heading" className="sr-only">
-            Products
-          </h2>
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
 
-          <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
-            {/* Filters */}
-            <form className="flex flex-col gap-2">
-              <h2 className="text-2xl">Filter</h2>
-              <div className="w-full border rounded p-4 mt-5">
-                <h2 className="text-lg">Categories</h2>
-                <div className="flex flex-col gap-4 mt-3">
-                  <div class="flex items-center">
-                    <input
-                      id="checked-checkbox"
-                      type="checkbox"
-                      value=""
-                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label for="checked-checkbox" class="ms-2 text-sm font-normal">
-                      Men
-                    </label>
-                  </div>
-                  <div class="flex items-center">
-                    <input
-                      id="checked-checkbox1"
-                      type="checkbox"
-                      value=""
-                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label for="checked-checkbox1" class="ms-2 text-sm font-normal">
-                      Women
-                    </label>
-                  </div>
-                  <div class="flex items-center">
-                    <input
-                      id="checked-checkbox2"
-                      type="checkbox"
-                      value=""
-                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label for="checked-checkbox2" class="ms-2 text-sm font-normal">
-                      Kids
-                    </label>
-                  </div>
-                </div>
-              </div>
-
-              <div className="w-full border rounded p-4 mt-5">
-                <h2 className="text-lg">Type</h2>
-                <div className="flex flex-col gap-4 mt-3">
-                  <div class="flex items-center">
-                    <input
-                      id="checked-checkbox"
-                      type="checkbox"
-                      value=""
-                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label for="checked-checkbox" class="ms-2 text-sm font-normal">
-                      Topwear
-                    </label>
-                  </div>
-                  <div class="flex items-center">
-                    <input
-                      id="checked-checkbox1"
-                      type="checkbox"
-                      value=""
-                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label for="checked-checkbox1" class="ms-2 text-sm font-normal">
-                      Bottomwaer
-                    </label>
-                  </div>
-                  <div class="flex items-center">
-                    <input
-                      id="checked-checkbox2"
-                      type="checkbox"
-                      value=""
-                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label for="checked-checkbox2" class="ms-2 text-sm font-normal">
-                      Winterwear
-                    </label>
-                  </div>
-                </div>
-              </div>
-            </form>
-
-            {/* Product grid */}
-            <div className="lg:col-span-3">
-              <ProductList />
-            </div>
+      {/* Filter Options */}
+      <div className='min-w-60'>
+        <p onClick={() => setShowFilter(!showFilter)} className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS
+          <img className={`h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`} src={assets.dropdown_icon} alt="" />
+        </p>
+        {/* Category Filter */}
+        <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
+          <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
+          <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+            <p className='flex gap-2'>
+              <input className='w-3' type="checkbox" value={'Men'} onChange={toggleCategory} /> Men
+            </p>
+            <p className='flex gap-2'>
+              <input className='w-3' type="checkbox" value={'Women'} onChange={toggleCategory} /> Women
+            </p>
+            <p className='flex gap-2'>
+              <input className='w-3' type="checkbox" value={'Kids'} onChange={toggleCategory} /> kids
+            </p>
           </div>
-        </section>
-      </main>
-  );
-};
+        </div>
+        {/* SubCategory Filter */}
+        <div className={`border border-gray-300 pl-5 py-3 my-5 ${showFilter ? '' : 'hidden'} sm:block`}>
+          <p className='mb-3 text-sm font-medium'>TYPE</p>
+          <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+            <p className='flex gap-2'>
+              <input className='w-3' type="checkbox" value={'Topwear'} onChange={toggleSubCategory} /> Topwear
+            </p>
+            <p className='flex gap-2'>
+              <input className='w-3' type="checkbox" value={'Bottomwear'} onChange={toggleSubCategory} /> Bottomwear
+            </p>
+            <p className='flex gap-2'>
+              <input className='w-3' type="checkbox" value={'Winterwear'} onChange={toggleSubCategory} /> Winterwear
+            </p>
+          </div>
+        </div>
+      </div>
 
-export default Collection;
+      {/* Right Side */}
+      <div className='flex-1'>
+
+        <div className='flex justify-between text-base sm:text-2xl mb-4'>
+          <Title text1={'ALL'} text2={'COLLECTIONS'} />
+          {/* Porduct Sort */}
+          <select onChange={(e) => setSortType(e.target.value)} className='border-2 border-gray-300 text-sm px-2'>
+            <option value="relavent">Sort by: Relavent</option>
+            <option value="low-high">Sort by: Low to High</option>
+            <option value="high-low">Sort by: High to Low</option>
+          </select>
+        </div>
+
+        {/* Map Products */}
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
+          {
+            filterProducts.map((item, index) => (
+              <ProductItem key={index} name={item.name} id={item._id} price={item.price} image={item.image} />
+            ))
+          }
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Collection
