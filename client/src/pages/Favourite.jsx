d163b02419a286e64efbbf339e0ebee40df08e06
daophuc2k3 <dodaophuc1111@gmail.com>
2024-11-01 10:32:35 +0700

    create favourite list page

--- client/src/pages/Favourite.jsx
+++ client/src/pages/Favourite.jsx
@@ -7,24 +7,23 @@ const DATA_TEMPLATE = [
     name: 'Men Round Neck Pure Cotton T-shirt',
     image: [assets.p_img1],
     price: '149$',
-    quanlity: 1,
   },
 ];
 
-const Cart = () => {
-  const [myCart, setMyCart] = useState([]);
+const Favourite = () => {
+  const [myFavourite, setMyFavourite] = useState([]);
 
   const onDeleteProduct = useCallback(
     (productId) => {
-      const data = myCart.filter((item) => item.id != productId);
-      setMyCart(data);
+      const data = myFavourite.filter((item) => item.id != productId);
+      setMyFavourite(data);
     },
-    [myCart]
+    [myFavourite]
   );
 
   useState(() => {
     //TODO: call api in here
-    setMyCart(DATA_TEMPLATE);
+    setMyFavourite(DATA_TEMPLATE);
   }, []);
 
   return (
@@ -32,14 +31,14 @@ const Cart = () => {
       <div class="container">
         <div className="flex gap-2 justify-start items-center">
           <h1 class="text-2xl font-semibold">
-            <span className="font-normal text-gray-700">Your</span> Cart
+            <span className="font-normal text-gray-700">Your</span> Favourite
           </h1>
           <div className="w-12 rounded h-[3px] mt-2 bg-gray-500"></div>
         </div>
         <div class="flex flex-col md:flex-row gap-4 border-t mt-8">
           <div class="w-full">
             <div class="bg-white rounded-lgp-6 mb-4">
-              {myCart.map((item) => {
+              {myFavourite.map((item) => {
                 return (
                   <div className="border-b flex justify-between items-center">
                     <div class="py-4">
@@ -56,7 +55,6 @@ const Cart = () => {
                         </div>
                       </div>
                     </div>
-
                     <div class="py-4">
                       <input
                         type="number"
@@ -68,7 +66,6 @@ const Cart = () => {
                         required
                       />
                     </div>
-
                     <div onClick={() => onDeleteProduct(item.id)}>
                       <img class="h-5 cursor-pointer w-5 rounded" src={assets.bin_icon} alt="Product image" />
                     </div>
@@ -83,7 +80,7 @@ const Cart = () => {
             <div class="rounded-lg p-6">
               <div className="flex mb-6 gap-2 justify-start items-center">
                 <h1 class="text-2xl font-semibold">
-                  <span className="font-normal text-gray-700">Cart</span> Totals
+                  <span className="font-normal text-gray-700">Favourite</span> Totals
                 </h1>
                 <div className="w-12 rounded h-[2px] mt-1 bg-gray-500"></div>
               </div>
@@ -98,7 +95,7 @@ const Cart = () => {
 
               <div class="flex justify-between my-4">
                 <span class="font-semibold">Total</span>
-                <span class="font-semibold">$21.98</span>
+                <span class="font-semibold">$19.99</span>
               </div>
               <button class="bg-black text-white py-2 px-4 rounded-lg mt-4 w-full">Proceed to checkout</button>
             </div>
@@ -109,4 +106,4 @@ const Cart = () => {
   );
 };
 
-export default Cart;
+export default Favourite;
