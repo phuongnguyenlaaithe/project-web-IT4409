ee0d9918ee0a560105f10cf7f632584889e44b31
daophuc2k3 <dodaophuc1111@gmail.com>
2024-11-09 12:40:03 +0700

    add apis homepage, collection, product

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,87 +1,100 @@
-import { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { ShopContext } from "../context/ShopContext";
-import { assets } from "../assets/assets";
-import RelatedProducts from "../components/RelatedProducts";
+import { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { ShopContext } from '../context/ShopContext';
+import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
   const { id } = useParams();
   const { products, currency, addToCart } = useContext(ShopContext);
 
-  const [product, setProduct] = useState(false)
-  const [image, setImage] = useState('')
-  const [size, setSize] = useState('')
+  const [product, setProduct] = useState(false);
+  const [image, setImage] = useState('');
+  const [size, setSize] = useState('');
 
   const fetchData = async () => {
     products.map((item) => {
-      if(item._id === id) {
-        setProduct(item)
-        setImage(item.image[0])
-        return null
+      if (item._id === id) {
+        setProduct(item);
+        setImage(item.image[0]);
+        return null;
       }
-    })
-  }
+    });
+  };
 
   useEffect(() => {
     fetchData();
     setSize('');
     window.scrollTo(0, 0);
-  },[id]);
-
+  }, [id]);
 
   return product ? (
     <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
-
       <div className="flex gap-12 flex-col sm:flex-row">
-          {/* group image product */}
+        {/* group image product */}
         <div className="flex flex-1 flex-col-reverse gap-3 sm:flex-row">
           <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
-            {
-              product.image.map((item , index) =>(
-                <img onClick={() => setImage(item)} key={index} src={item} alt="" className="w-[24%] cursor-pointer sm:w-full sm:mb-3 flex-shrink-0" />
-              ))
-            }
+            {product.image.map((item, index) => (
+              <img
+                onClick={() => setImage(item)}
+                key={index}
+                src={item}
+                alt=""
+                className="w-[24%] cursor-pointer sm:w-full sm:mb-3 flex-shrink-0"
+              />
+            ))}
           </div>
           <div className="w-full sm:w-[80%]">
             <img src={image} className="w-full h-auto" alt="" />
           </div>
         </div>
 
-          {/* info product */}
+        {/* info product */}
         <div className="flex-1">
-            <h1 className="font-medium text-2xl">{product.name}</h1>
-            <div className="flex gap-1 mt-4 items-center">
-              <img src={assets.star_icon} alt="" className="w-3"/>
-              <img src={assets.star_icon} alt="" className="w-3"/>
-              <img src={assets.star_icon} alt="" className="w-3"/>
-              <img src={assets.star_icon} alt="" className="w-3"/>
-              <img src={assets.star_dull_icon} alt="" className="w-3"/>
-              <p className="pl-2">(86)</p>
+          <h1 className="font-medium text-2xl">{product.name}</h1>
+          <div className="flex gap-1 mt-4 items-center">
+            <img src={assets.star_icon} alt="" className="w-3" />
+            <img src={assets.star_icon} alt="" className="w-3" />
+            <img src={assets.star_icon} alt="" className="w-3" />
+            <img src={assets.star_icon} alt="" className="w-3" />
+            <img src={assets.star_dull_icon} alt="" className="w-3" />
+            <p className="pl-2">(86)</p>
+          </div>
+          <p className="font-medium text-3xl mt-5">
+            {currency}
+            {product.price}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-[80%]">{product.description}</p>
+          <div className="flex flex-col gap-3 my-8">
+            <p>Select Size</p>
+            <div className="flex gap-2">
+              {product.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  key={index}
+                  className={`border py-2 px-4 bg-gray-100 ${item === size ? 'bg-orange-400' : ''}`}
+                >
+                  {item}
+                </button>
+              ))}
             </div>
-            <p className="font-medium text-3xl mt-5">{currency}{product.price}</p>
-            <p className="mt-5 text-gray-500 md:w-[80%]">{product.description}</p>
-            <div className="flex flex-col gap-3 my-8">
-              <p>Select Size</p>
-              <div className="flex gap-2">
-                {
-                  product.sizes.map((item, index) => (
-                    <button onClick={()=>setSize(item)} key={index} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'bg-orange-400' : '' }`}>{item}</button>
-                  ))
-                }
-              </div>
-            </div>
-            <button onClick={()=>addToCart(product._id, size)} className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">ADD TO CART</button>
-            <hr className="my-8 sm:w-4/5"/>
-            <div className="flex flex-col gap-1 text-gray-500 text-sm ">
-              <p>100% Original product.</p>
-              <p>Cash on delivery is available on this product.</p>
-              <p>Easy return and exchange policy within 7 days.</p>
-            </div>
+          </div>
+          <button
+            onClick={() => addToCart(product._id, size)}
+            className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700"
+          >
+            ADD TO CART
+          </button>
+          <hr className="my-8 sm:w-4/5" />
+          <div className="flex flex-col gap-1 text-gray-500 text-sm ">
+            <p>100% Original product.</p>
+            <p>Cash on delivery is available on this product.</p>
+            <p>Easy return and exchange policy within 7 days.</p>
+          </div>
         </div>
       </div>
 
       <div className="mt-20">
-
         <div className="flex">
           <b className="py-3 px-4 border text-sm">Description</b>
           <p className="py-3 px-4 border text-sm">Reviews (66)</p>
@@ -89,19 +102,25 @@ const Product = () => {
 
         <div className="flex flex-col gap-4 border p-5 text-sm text-gray-500 ">
           <p>
-          An e-commerce website is an online platform that facilitates the buying and selling of products or services over the internet. It serves as a virtual marketplace where businesses and individuals can showcase their products, interact with customers, and conduct transactions without the need for a physical presence. E-commerce websites have gained immense popularity due to their convenience, accessibility, and the global reach they offer.
+            An e-commerce website is an online platform that facilitates the buying and selling of products or services
+            over the internet. It serves as a virtual marketplace where businesses and individuals can showcase their
+            products, interact with customers, and conduct transactions without the need for a physical presence.
+            E-commerce websites have gained immense popularity due to their convenience, accessibility, and the global
+            reach they offer.
           </p>
           <p>
-          E-commerce websites typically display products or services along with detailed descriptions, images, prices, and any available variations (e.g., sizes, colors). Each product usually has its own dedicated page with relevant information.
+            E-commerce websites typically display products or services along with detailed descriptions, images, prices,
+            and any available variations (e.g., sizes, colors). Each product usually has its own dedicated page with
+            relevant information.
           </p>
         </div>
-
       </div>
 
-      <RelatedProducts category={product.category} subCategory={product.subCategory}/>
-
+      <RelatedProducts category={product.category} subCategory={product.subCategory} />
     </div>
-  ) : <div className="opacity-0"></div>
+  ) : (
+    <div className="opacity-0"></div>
+  );
 };
 
 export default Product;
