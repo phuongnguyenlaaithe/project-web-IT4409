a6fa1e14915a5918019b7777995fdecabaf8e70a
Phuc_Phung <minhphuchc1308@gmail.com>
2024-12-13 01:22:11 +0700

    add search for admin

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -9,6 +9,8 @@ const Orders = ({token}) => {
   const [orders, setOrders] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(5);
+  const [search, setSearch] = useState('');
+  const [filterOrders, setFilterOrders] = useState([]);
 
   const getAllOrders = async () => {
     if(!token) {
@@ -50,10 +52,28 @@ const Orders = ({token}) => {
   useEffect(() => {
     getAllOrders();
   }, [token]);
+  
+
+  const applyFilter = () => {
+    let ordersCopy = orders.slice();
+
+    if (search) {
+      ordersCopy = ordersCopy.filter((item) => item._id.toLowerCase().includes(search.toLowerCase()));
+    }
+
+    setFilterOrders(ordersCopy);
+  };
+  
+  useEffect(() => {
+    if (orders.length > 0) {
+      applyFilter();
+      setCurrentPage(1);
+    }
+  }, [orders, search]);
 
   const indexOfLastOrder = currentPage * itemsPerPage;
   const indexOfFirstOrder = indexOfLastOrder - itemsPerPage;
-  const currentOrders = orders.slice(indexOfFirstOrder, indexOfLastOrder);
+  const currentOrders = filterOrders.slice(indexOfFirstOrder, indexOfLastOrder);
 
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
@@ -66,42 +86,70 @@ const Orders = ({token}) => {
 
 
   return (
-    <div className='my-8'>
-      <div className='flex justify-between items-center mb-4'>
+    <div className="my-8">
+      <div className="flex flex-col sm:flex-row gap-2 justify-between items-center mb-4">
         <h3>Order List</h3>
+        <div>
+          <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 rounded-full">
+            <input
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              className="flex-1 outline-none bg-inherit text-sm border-none"
+              type="text"
+              placeholder="Search"
+            />
+            <img className="w-4" src={assets.search_icon} alt="" />
+          </div>
+        </div>
       </div>
 
       <div>
         {currentOrders.map((order, index) => (
-          <div className='grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700' key={index}>
-            <img className='w-12' src={assets.parcel_icon} alt="" />
+          <div
+            className="grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700"
+            key={index}
+          >
+            <img className="w-12" src={assets.parcel_icon} alt="" />
             <div>
               <div>
                 {order.items.map((item, index) => {
-                  if(index === order.items.length - 1) {
-                    return <p className='py-0.5' key={index}>{item.name} x {item.quantity} <span>{item.size}</span></p>
+                  if (index === order.items.length - 1) {
+                    return (
+                      <p className="py-0.5" key={index}>
+                        {item.name} x {item.quantity} <span>{item.size}</span>
+                      </p>
+                    );
                   } else {
-                    return <p className='py-0.5' key={index}>{item.name} x {item.quantity} <span>{item.size}</span> ,</p>
+                    return (
+                      <p className="py-0.5" key={index}>
+                        {item.name} x {item.quantity} <span>{item.size}</span> ,
+                      </p>
+                    );
                   }
                 })}
               </div>
-              <p className='my-3 font-medium'>{order.address.name}</p>
+              <p className="my-3 font-medium">{order.address.name}</p>
               <div>
                 <p>{order.address.address}</p>
               </div>
               <p>{order.address.phone}</p>
             </div>
             <div>
-              <p className='text-sm sm:text-base'>Items: {order.items.length}</p>
-              <p className='mt-3'>Method: {order.paymentMethod}</p>
-              <p>Payment: {order.payment ? 'Done' : 'Pending'}</p>
+              <p className="text-sm sm:text-base">
+                Items: {order.items.length}
+              </p>
+              <p className="mt-3">Method: {order.paymentMethod}</p>
+              <p>Payment: {order.payment ? "Done" : "Pending"}</p>
               <p>Date: {new Date(order.date).toLocaleDateString()}</p>
             </div>
-            <p className='text-sm sm:text-base'>{currency}{order.amount}</p>
-            <select 
-              onChange={(e) => handleStatus(e, order._id)} 
-              value={order.status} 
-              className='p-2 font-semibold'
+            <p className="text-sm sm:text-base">
+              {currency}
+              {order.amount}
+            </p>
+            <select
+              onChange={(e) => handleStatus(e, order._id)}
+              value={order.status}
+              className="p-2 font-semibold"
             >
               <option value="Order Placed">Order Placed</option>
               <option value="Packing">Packing</option>
@@ -114,19 +162,19 @@ const Orders = ({token}) => {
       </div>
 
       {/* Pagination Controls */}
-      {orders.length > 0 && (
-        <Pagination 
+      {filterOrders.length > 0 && (
+        <Pagination
           currentPage={currentPage}
           onPageChange={handlePageChange}
           itemsPerPage={itemsPerPage}
           onItemsPerPageChange={handleItemsPerPageChange}
-          totalItems={orders.length}
+          totalItems={filterOrders.length}
           currentPageFirstItem={indexOfFirstOrder + 1}
-          currentPageLastItem={Math.min(indexOfLastOrder, orders.length)}
+          currentPageLastItem={Math.min(indexOfLastOrder, filterOrders.length)}
         />
       )}
     </div>
-  )
+  );
 }
 
 export default Orders
